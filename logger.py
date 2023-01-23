9c3b3236eb8d862994a9ceebafb010ff2c094eb5
sasha42 <sasha42@users.noreply.github.com>
2023-01-23 09:55:59 +0100

    Use environment variables

--- logger.py
+++ logger.py
@@ -8,8 +8,8 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 
 # Influx DB config
 token = os.environ.get("INFLUXDB_TOKEN")
-org = "Sasha"
-url = "https://eu-central-1-1.aws.cloud2.influxdata.com"
+org = os.environ.get("INFLUXDB_ORG")
+url = os.environ.get("INFLUXDB_URL")
 client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 bucket="temperature"
 write_api = client.write_api(write_options=SYNCHRONOUS)
@@ -20,10 +20,10 @@ def write_to_influxdb(temperature, location):
      .field("realtemp", temperature)
      .tag("location", location)
    )
-   write_api.write(bucket=bucket, org="Sasha", record=point)
+   write_api.write(bucket=bucket, org=org, record=point)
 
 def get_sensors():
-    b = Bridge('192.168.1.200')
+    b = Bridge(os.environ.get("PHILIPS_HUE_IP"))
     b.connect()
     upstairs = b.get_sensor()['14']['state']['temperature']
     downstairs = b.get_sensor()['61']['state']['temperature']
